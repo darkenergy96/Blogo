84a762dc67cd2bf7a6bef2d86152cc7428e40e7a
Sumanth Reddy <srksumanth@gmail.com>
2017-03-27 21:51:24 +0530

    Delete api_keys.js

--- api_keys.js
+++
@@ -1,7 +0,0 @@
-var api = {
-    google:{
-        clientID: "869989273067-ocpvss4e2pggck34fc6glgab805sk5e2.apps.googleusercontent.com",
-        clientSecret: "B_qiYZXKh3P5OYTo0dIFrhlg"
-    }
-};
-module.exports = api;
